41413fac605c86dd3d24f9093c3ff93bc79b42f9
Julie Nisbet <3282364+julienisbet@users.noreply.github.com>
2022-12-02 07:10:35 -0800

    WIP - setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,23 +1,70 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS workouts_sets;
+DROP TABLE IF EXISTS sets_parts;
+DROP TABLE IF EXISTS workouts;
+DROP TABLE IF EXISTS swim_sets;
+DROP TABLE IF EXISTS parts;
 
-DROP TABLE IF EXISTS tasks;
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
+CREATE TABLE workouts (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR,
-  password_hash VARCHAR NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL
-);
-
-CREATE TABLE todos (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id BIGINT,
-  completed BOOLEAN NOT NULL DEFAULT(FALSE),
+  name VARCHAR,
   description VARCHAR,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  image VARCHAR
 );
+
+CREATE TABLE swim_sets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  description VARCHAR NOT NULL
+);
+
+CREATE TABLE parts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  qty INT NOT NULL,
+  distance INT NOT NULL,
+  interval VARCHAR,
+  detail VARCHAR,
+);
+
+CREATE TABLE workouts_sets(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  workout_id BIGINT,
+  swim_set_id BIGINT,
+  repeat INT,
+  FOREIGN KEY (workout_id) REFERENCES workouts(id),
+  FOREIGN KEY (swim_set_id) REFERENCES swim_sets(id),
+);
+
+CREATE TABLE  swim_sets_parts(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  swim_set_id BIGINT,
+  parts_id BIGINT,
+  FOREIGN KEY(swim_set_id) REFERENCES swim_set_id(id),
+  FOREIGN KEY(parts_id) REFERENCES parts_id(id)
+);
+
+INSERT INTO workouts (name, description, image) VALUES (
+  'Ravioli Starfish', 'Mostly free - short distances', 'https://images.squarespace-cdn.com/content/v1/5aa560f95b409b3000db7c5d/1631809696742-89QVMOKAWLDHIHGLOGPV/ravioli-starfish.jpg?format=1500w' 
+);
+
+INSERT INTO swim_sets (description) VALUES ('Warm Up'), ('Main Set'), ('Warm Down');
+
+INSERT INTO parts (qty, distance, interval, detail) VALUES 
+(3, 50, NULL, 'Kick'),
+(1, 50, NULL, 'Drill'),
+(1, 100, NULL, 'Swim'),
+(6, 100, 'Kick Base', 'kick descend 1-3, 4-6'),
+(8, 50, 'Base + 20', '2x through: buildup, build down, easy, fast');
+
+INSERT INTO swim_sets_parts (swim_set_id, parts_id) VALUES 
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5);
+
+INSERT INTO workouts_sets (workout_id, swim_set_id, repeat) VALUES 
+(1, 1, 1),
+(1, 2, 2),
+(1, 3, 1);
